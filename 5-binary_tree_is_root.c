fc31cc09a554ea8cc6599f89dda372a0b8ed7e7b
stellarAlien <saifgaida@gmail.com>
2022-03-23 21:17:30 +0100

    5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,12 +1,12 @@
 #include"binary_trees.h"
 /**
 *binary_tree_is_root - checks if node is root
-*@node: node to check 
-*Return: 1 if root else 0 
+*@node: node to check
+*Return: 1 if root else 0
 */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if(node->parent == NULL)
-		return 1;
-	return 0;
+	if (node->parent == NULL || node == NULL)
+		return (1);
+	return (0);
 }
