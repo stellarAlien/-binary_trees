c28e875979d4571510e412b738cbf686aa0b04e4
stellarAlien <saifgaida@gmail.com>
2022-03-23 22:16:02 +0100

    4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,12 +2,11 @@
 /**
  *binary_tree_is_leaf - checks if a node in a bt is a leaf
  * @node: node to check if leaf
- * @return 1 if leaf else 0
  * Return: 1 if leaf else 0
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->right || node->left  || node == NULL)
+	if (node == NULL || node->right || node->left)
 		return (0);
 	return (1);
 }
