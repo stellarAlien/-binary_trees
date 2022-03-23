db821d4c4c64119b820666a3a4f768c54f9a06fd
stellarAlien <saifgaida@gmail.com>
2022-03-23 21:58:56 +0100

    3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,10 +1,14 @@
 #include"binary_trees.h"
- void binary_tree_delete(binary_tree_t *tree)
- {
-	if(tree == NULL)
+/**
+ * binary_tree_delete - deletes a tree
+ * @tree: root
+ */
+	void binary_tree_delete(binary_tree_t *tree)
+	{
+	if (tree == NULL)
 		return;
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
 	free(tree);
 	tree = NULL;
- }
+	}
