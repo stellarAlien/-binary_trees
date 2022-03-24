2ba6053c71a17aac4cb60da63524b50abe3d5813
stellarAlien <saifgaida@gmail.com>
2022-03-24 18:36:25 +0100

    18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -21,7 +21,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if(node == NULL || node->parent == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 	return (binary_tree_sibling(node->parent));
 }
