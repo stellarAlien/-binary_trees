6e4a730de067598c71c974f2f220fbcfbc79dadd
stellarAlien <saifgaida@gmail.com>
2022-03-24 18:25:37 +0100

    uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,25 @@
+#include"binary_trees.h"
+/**
+ * binary_tree_sibling - check sibling of node
+ *
+ * @node: binary_tree_t *
+ * Return: binary_tree_t*
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
+}
+/**
+ * binary_tree_uncle - sibling of parent node
+ *
+ * @node: binary_tree_t *
+ * Return: binary_tree_t*
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	return (binary_tree_sibling(node->parent));
+}
