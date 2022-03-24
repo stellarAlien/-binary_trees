de8006c6ab83dbf0f41a92c3b8f50a31e56c02ba
stellarAlien <saifgaida@gmail.com>
2022-03-24 03:14:36 +0100

    15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,16 @@
+#include"binary_trees.h"
+/**
+ * binary_tree_is_full - checks if tree is full (elements count = 2^h)
+ * @tree: node
+ * Return: int
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (binary_tree_is_full(tree->left) &&
+	binary_tree_is_full(tree->right));
+}
