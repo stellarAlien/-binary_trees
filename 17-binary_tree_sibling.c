b5dbc0ecdb1ba8cad5c4b2ddad7964e777bdb1df
stellarAlien <saifgaida@gmail.com>
2022-03-24 18:19:51 +0100

    siblings

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,15 @@
+#include"binary_trees.h"
+/**
+ * binary_tree_sibling - check sibling of node
+ *
+ * @node: binary_tree_t *
+ * Return: binary_tree_t*
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
+}
