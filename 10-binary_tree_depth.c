9a856f391d5efab7112223e539f122f1bc2925e2
stellarAlien <saifgaida@gmail.com>
2022-03-25 11:19:12 +0100

    10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,13 +6,7 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t i = 0;
-
-	if (!tree)
-		return (-1);
-	if (!tree->parent)
-		return (i);
-	else
-		return (++i + binary_tree_depth(tree->parent));
-	return (i);
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
+	return (binary_tree_depth(tree->parent) + 1);
 }
